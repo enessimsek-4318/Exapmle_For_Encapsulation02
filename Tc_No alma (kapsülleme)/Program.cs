bc076e296ba51239644e0af641ec02975e12e1a5
enessimsek-4318 <101976848+enessimsek-4318@users.noreply.github.com>
2022-05-15 11:29:47 +0300

    Example_For_Encapsulation02

---
+++ Tc_No alma (kapsülleme)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tc_No_alma__kapsülleme_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen Tc Kimlik Numaranızı Giriniz:");
+            string tcGiris = Console.ReadLine();
+            personel p1 = new personel();
+            p1.TC = tcGiris;
+            Console.WriteLine("TC Kimlik Numaranız: " + p1.TC);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tc_No alma (kapsülleme)/personel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tc_No_alma__kapsülleme_
+{
+    class personel
+    {
+        private string tc;
+        public string TC 
+        {
+            get
+            {
+                return tc.Substring(0, 5)+"******";
+            }
+            set
+            {
+                
+                if (value.Length==11)
+                {
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        bool sayimi =char.IsNumber(value[i]);
+                        if (sayimi)
+                        {
+                            tc = value;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Geçersiz Karakter!!");
+                        }
+                    }
+                   
+                }
+                else
+                {
+                    Console.WriteLine("Girmiş Olduğunuz Tc Kimlik No 11 Haneli Değildir.");
+                }
+            } 
+        
+        
+        
+        }
+
+    }
+}
